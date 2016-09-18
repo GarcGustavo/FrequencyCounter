a9fedd8609e2008965652c99a142db0ad6147084
Gustavo <gustavo-reyes@live.com>
2016-09-18 19:09:34 -0400

    Finished project, haven't tested yet

--- src/approachClasses/MapFD.java
+++ src/approachClasses/MapFD.java
@@ -1,6 +1,8 @@
 package approachClasses;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Map.Entry;
 
 public class MapFD<E extends Comparable<E>> extends FrequencyCounter<E> {
@@ -11,8 +13,26 @@ public class MapFD<E extends Comparable<E>> extends FrequencyCounter<E> {
 
 	@Override
 	public ArrayList<Entry<E, Integer>> computeFDList(ArrayList<E> dataSet) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		HashMap< E, Integer > mapList = new HashMap<E, Integer>();
+		ArrayList<Map.Entry<E, Integer>> results = new ArrayList<Map.Entry<E, Integer>>();
+		
+		for(int i= 0; i<dataSet.size();i++ )
+		{
+			if(mapList.containsKey(dataSet.get(i)))
+			{
+				mapList.put(dataSet.get(i), mapList.get(dataSet.get(i)) + 1);
+			}
+			else
+			{
+				mapList.put(dataSet.get(i), 1);
+			}
+		}
+		
+		for (Entry<E,Integer> entry : mapList.entrySet()){ 
+				results.add(entry);
+		}
+		return results;
 	}
 
 }
