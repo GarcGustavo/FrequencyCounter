bd440f7f8f97eaad9c196cc5b5adfae33c2e3641
Gustavo <gustavo-reyes@live.com>
2016-09-18 21:10:52 -0400

    Everything working as it should, need to comment.

--- src/experimentClasses/ExperimentalTrials.java
+++ src/experimentClasses/ExperimentalTrials.java
@@ -8,48 +8,64 @@ import java.io.PrintWriter;
 
 public class ExperimentalTrials {
 
-	private static int elements=50; //number of entries in list
-	private static int repetitions=200; //times to repeat each approach in one list
-	private static int increments=30; //amount of lists to go through
+	private static int entries=50; //number of starting elements in list
+	private static int repetitions=20; //times to repeat each approach in one list
+	private static int iterations=30; //amount of lists to go through
 	private static int elementIncrement=50; //amount of elements to add for next list experiment
 	private static ArrayList<Integer> intList = new ArrayList<Integer>();
 	private static ArrayList<String> strList = new ArrayList<String>();
 
 
 	public static void main(String[] args) throws IOException{
-		
-		clearFile("resultsSequential.txt");
-		clearFile("resultsOrdered.txt");
-		clearFile("resultsMap.txt");
-		for(int i=1; i<=increments; i++){
-		experimentSequen.testerInt(intList, repetitions);
-		experimentOrdered.testerInt(intList, repetitions);
-		experimentMap.testerInt(intList, repetitions);
+
+		clearFile("experimentalResults/resultsSequential.txt");
+		clearFile("experimentalResults/resultsOrdered.txt");
+		clearFile("experimentalResults/resultsMap.txt");
+		int index=0;
+		intList.clear();
+		strList.clear();
+		for(int i=1; i<=iterations; i++){
+			if(i<=1){
+				createList(index, entries);
+			}
+			else{
+				createList(index, elementIncrement);
+			}
+			experimentSequen.testerInt(intList, repetitions);
+			experimentOrdered.testerInt(intList, repetitions);
+			experimentMap.testerInt(intList, repetitions);
+			index+=elementIncrement;
 		}
-		for(int i=1; i<=increments; i++){
-		createList(elements);
-		experimentSequen.testerStr(strList, repetitions);
-		experimentOrdered.testerStr(strList, repetitions);
-		experimentMap.testerStr(strList, repetitions);
-		elements+=elementIncrement;
+		index=0;
+		intList.clear();
+		strList.clear();
+		for(int i=1; i<=iterations; i++){
+			if(i<=1){
+				createList(index, entries);
+			}
+			else{
+				createList(index, elementIncrement);
+			}
+			experimentSequen.testerStr(strList, repetitions);
+			experimentOrdered.testerStr(strList, repetitions);
+			experimentMap.testerStr(strList, repetitions);
+			index+=elementIncrement;
 		}
-		
 		System.out.println("Experiment complete, data saved to experimentalResults directory text files.");
 	}
 
 	//Method to clear files of previous experiment results
 	public static void clearFile(String fileName) throws FileNotFoundException{
 		PrintWriter pw = new PrintWriter(fileName);
+		pw.write("");
 		pw.close();
 	}
-	
-	public static void createList(int numElems){
+
+	public static void createList(int index, int numElems){
 		Random randomGen = new Random();
 		for(int i=0; i<numElems; i++){
-			intList.add(randomGen.nextInt(numElems/2));
-		}
-		for(int i=0; i<numElems; i++){	
-			strList.add(String.valueOf(randomGen.nextInt(numElems/2))+"\n");
+			intList.add(index, randomGen.nextInt(numElems/2));
+			strList.add(index, String.valueOf(randomGen.nextInt(numElems/2))+"\n");
 		}
 	}
 }
